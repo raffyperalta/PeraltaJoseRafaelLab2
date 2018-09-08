7f382a965cb6f2d9bb210f039af19858d8367bd1
Raffy Peralta <raffy.peralta.01@gmail.com>
2018-09-08 11:19:43 +0800

    Initial commit

---
+++ app/src/main/java/joserafael/peralta/com/peraltajoserafael_lab2/MainActivity.java
@@ -0,0 +1,13 @@
+package joserafael.peralta.com.peraltajoserafael_lab2;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.constraintlayout);
+    }
+}
